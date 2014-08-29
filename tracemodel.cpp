aa269c933459127355d5bb582fd101ddfb778b7e
Robin Burchell <robin+git@viroteck.net>
2014-08-29 11:42:49 +0200

    Parse cpu_frequency events too.

--- tracemodel.cpp
+++ tracemodel.cpp
@@ -10,29 +10,40 @@ TraceModel *TraceModel::fromFile(QFile *f)
         QByteArray line = f->readLine();
         TraceEvent te = TraceEvent::fromString(line);
         if (te.isValid()) {
-            if (te.eventName() == "cpu_idle") {
-                // Events look like:
-                // TraceEvent(0 117925.355823 "<idle>" 1 "cpu_idle" "state=3 cpu_id=1")
-                QList<QString> datas = te.details().split(" ");
-                QMap<QString, QString> params;
+            // do the general processing first
+            QMap<QString, QString> params;
 
+            if (te.eventName() == "cpu_idle" ||
+                te.eventName() == "cpu_frequency") {
+                QList<QString> datas = te.details().split(" ");
                 foreach (const QString &data, datas) {
                     QList<QString> param = data.split("=");
-                    if (param.length() == 0) {
-                        qWarning() << "Empty param for line " << line;
+                    if (param.length() < 2) {
+                        qWarning() << "Bad param for line " << line;
                         continue;
                     }
 
-                    // state 4294967295 is secretly -1 in an unsigned int which
-                    // means "exit the current state back to state 0"
-                    if (param[0] == "state" && param[1] == "4294967295")
-                        param[1] = "0";
-
                     params[param[0]] = param[1];
                 }
+            }
 
-                // params should now look like:
+            if (te.eventName() == "cpu_idle") {
+                // Events look like:
+                // TraceEvent(0 117925.355823 "<idle>" 1 "cpu_idle" "state=3 cpu_id=1")
+
+                // state 4294967295 is secretly -1 in an unsigned int which
+                // means "exit the current state back to state 0"
+                if (params["state"] == "4294967295")
+                    params["state"] = "0";
+
+                // params looks like:
                 // QMap(("cpu_id", "1")("state", "0"))
+            } else if (te.eventName() == "cpu_frequency") {
+                // Events look like:
+                // TraceEvent(17742 117943.296614 "kworker/1:0" 1 "cpu_frequency" "state=918000 cpu_id=1")
+
+                // params looks like:
+                // QMap(("cpu_id", "1")("state", "918000"))
             } else {
                 qDebug() << te;
             }
