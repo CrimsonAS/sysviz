0aec63ba240869471c026beb5babf54b08a64d9e
Robin Burchell <robin+git@viroteck.net>
2014-08-29 11:32:24 +0200

    Add parsing of cpu_idle events.

--- tracemodel.cpp
+++ tracemodel.cpp
@@ -9,8 +9,34 @@ TraceModel *TraceModel::fromFile(QFile *f)
     while (!f->atEnd()) {
         QByteArray line = f->readLine();
         TraceEvent te = TraceEvent::fromString(line);
-        if (te.isValid())
-            qDebug() << te;
+        if (te.isValid()) {
+            if (te.eventName() == "cpu_idle") {
+                // Events look like:
+                // TraceEvent(0 117925.355823 "<idle>" 1 "cpu_idle" "state=3 cpu_id=1")
+                QList<QString> datas = te.details().split(" ");
+                QMap<QString, QString> params;
+
+                foreach (const QString &data, datas) {
+                    QList<QString> param = data.split("=");
+                    if (param.length() == 0) {
+                        qWarning() << "Empty param for line " << line;
+                        continue;
+                    }
+
+                    // state 4294967295 is secretly -1 in an unsigned int which
+                    // means "exit the current state back to state 0"
+                    if (param[0] == "state" && param[1] == "4294967295")
+                        param[1] = "0";
+
+                    params[param[0]] = param[1];
+                }
+
+                // params should now look like:
+                // QMap(("cpu_id", "1")("state", "0"))
+            } else {
+                qDebug() << te;
+            }
+        }
     }
 
     return new TraceModel;
